9dd8f60e7f880f3fc32b3ea2e2790b675f823720
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-10-22 21:39:23 -0300

    Update script.js

--- 05/script.js
+++ 05/script.js
@@ -3,3 +3,18 @@
   
   Após capturar os 10 items, imprima-os, separando por vírgula.
 */
+
+
+function createList() {
+  let listCart = []
+
+  for (var i = 0; i < 10; i++) {
+    let itemCart = prompt('Digite um item da lista')
+    listCart.push(`${itemCart}`)
+  }
+
+  return alert(listCart.join(', '))
+}
+
+createList()
+
